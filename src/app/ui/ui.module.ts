e26ffd36ddf9b26d4575d21c6b580f26da51ad76
0b10 <0b10@pm.me>
2020-02-10 21:38:11 +0000

    build(angular): upgrade to v9
      Summary
        Upgrade dependencies, and fix any broken code.
    
      Notes
        A dep requires localizations, which seem to have changed in v9.
    
      Project
        + add @angular/localization to satisfy a dep requirement
        + yarn upgrade all deps
        + remove broken imports
        + upgrade to 9
        + upgrade local versions to latest

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,13 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ElementWidthDirective } from './directives/element-width.directive';
 import { LoadingModule } from './loading/loading.module';
 import { PostModule } from './post/post.module';
-import { ElementWidthService } from './services/element-width.service';
 
 @NgModule({
   imports: [CommonModule, PostModule],
   exports: [PostModule, LoadingModule],
-  declarations: [ElementWidthDirective, ElementWidthService],
 })
 export class UiModule {}
